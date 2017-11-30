d22cb8ea48a8de3109f0757f4e3e56961092f94a
Oscar <ocrozo@gmail.com>
2017-11-30 16:07:21 +0000

    added started

---
+++ python/start-game.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sleep 5
+PYTHONPATH=".:/home/pi/rpi_ws281x_game/python:/home/pi/rpi_ws281x_game/python/build/lib.linux-arm7l-2.7"
+python /home/pi/rpi_ws281x_game/python/examples/androidgame.py
